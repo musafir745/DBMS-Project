43ae26647abfd50c6c512bc027091c2bb0923787
A T M Fayjur Rahman  Adil <230506761+musafir745@users.noreply.github.com>
2026-07-24 02:34:10 +0600

    Delete utils directory

--- utils/ExpressError.js
+++
@@ -1,9 +0,0 @@
-class ExpressError extends Error{
-    constructor(statuscode , message){
-        super();
-        this.statuscode = statuscode;
-        this.message = message;
-    }
-}
-
-module.exports = ExpressError
